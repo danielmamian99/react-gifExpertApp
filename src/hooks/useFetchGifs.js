68337db894ed979a37ce11837035e77e0aec52b6
Daniel Mamian <danimaster99@gmail.com>
2021-12-09 01:06:14 -0500

    error fetch actualizado

--- src/hooks/useFetchGifs.js
+++ src/hooks/useFetchGifs.js
@@ -1,6 +1,5 @@
-import { useState } from "react"
-import { useEffect } from "react/cjs/react.development";
-import {getGifs} from '../helpers/getGifs'
+import { useState, useEffect  } from "react";
+import {getGifs} from '../helpers/getGifs';
 
 export const useFetchGifs = ( category ) =>{
     const [state, setState] = useState({
